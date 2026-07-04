5c8bc8f694bb92ac7ce4357ddf36625060a96ffa
hilman-nidal-hamzi <hilmanhamzi@gmail.com>
2026-07-04 17:28:31 +0700

    chore: remove legacy case study pages, diagrams, and UI components

--- src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button }       from './Button'
-export { Badge }        from './Badge'
-export { Tag }          from './Tag'
-export { SectionLabel } from './SectionLabel'
-export { ProjectCard }  from './ProjectCard'

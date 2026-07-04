c5e68b6f7cb70996366c08f40fa957ed210d3248
hilman-nidal-hamzi <hilmanhamzi@gmail.com>
2026-07-04 11:34:13 +0700

    chore: remove legacy section components from previous design

--- src/components/sections/index.ts
+++
@@ -1,8 +0,0 @@
-export { Hero }         from './Hero'
-export { Stats }        from './Stats'
-export { About }        from './About'
-export { Stack }        from './Stack'
-export { Projects }     from './Projects'
-export { Experience }   from './Experience'
-export { Achievements } from './Achievements'
-export { Contact }      from './Contact'

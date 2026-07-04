b38f4f1027933928f247e770c63e6d38aea9ab21
hilman-nidal-hamzi <hilmanhamzi@gmail.com>
2026-07-05 02:02:20 +0700

    feat: restore Testimonials and LogoCloud sections in correct sequence order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { About } from '@/components/sections/About'
 import { Work } from '@/components/sections/Work'
 import { Craft } from '@/components/sections/Craft'
 import { History } from '@/components/sections/History'
-
+import { Testimonials } from '@/components/sections/Testimonials'
+import { LogoCloud } from '@/components/sections/LogoCloud'
 import { CTA } from '@/components/sections/CTA'
 import { Footer } from '@/components/sections/Footer'
 
@@ -15,7 +16,8 @@ export default function Home() {
       <Work />
       <Craft />
       <History />
-
+      <Testimonials />
+      <LogoCloud />
       <CTA />
       <Footer />
     </main>
